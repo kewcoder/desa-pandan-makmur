c8d729357850fe7fe8faf1a08c0126873c725093
Ari bahtiar <kewcoder@gmail.com>
2021-08-23 08:00:18 +0700

    .

--- assets/pandan-makmur.js
+++ assets/pandan-makmur.js
@@ -45,6 +45,43 @@ axios.get('/data-desa.json')
     // console.log(response);
     let dataDesa = response.data
     console.log(dataDesa)
+
+    let listDusun = ``;
+    dataDesa.datadusun.forEach(dusun => {
+
+    let dataDusun = `
+      <div class="dusun">
+                  <div class="data-dusun">
+                    <h4> Dusun <br> ${dusun.nama_dusun}</h4>
+                    <br>
+                    <div>
+                    <b>${dusun.jumlah_penduduk} </b> Penduduk <br>
+                    <b>${dusun.jumlah_laki_laki} </b> Laki-laki <br>
+                    <b>${dusun.jumlah_perempuan} </b> Perempuan  <br>
+                    <b>${dusun.jumlah_keluarga} </b> Keluarga
+                      </div>
+                  </div>
+              </div>
+  `
+      listDusun += dataDusun
+    });
+
+    document.querySelector(".list-dusun").innerHTML = listDusun
+
+    let listTotalPenduduk = `
+      <div class="list-item">
+          <strong>${dataDesa.dataseluruh.jumlah_laki_laki}</strong>  Laki-laki
+      </div>
+      <div class="list-item">
+          <strong>${dataDesa.dataseluruh.jumlah_perempuan}</strong> Perempuan
+      </div>
+      <div class="list-item">
+          <strong>${dataDesa.dataseluruh.jumlah_penduduk}</strong> Total Penduduk
+      </div>
+      <div class="list-item">
+          <strong>${dataDesa.dataseluruh.jumlah_keluarga}</strong>  Keluarga
+      </div>`
+      document.querySelector(".list-total-penduduk").innerHTML = listTotalPenduduk
   })
   .catch(function (error) {
     // handle error
